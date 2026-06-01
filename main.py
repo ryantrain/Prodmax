c83a36c4256c7645fe7a2ad8dbfab637a860d41c
Ryan Tran <188256973+ryantrain@users.noreply.github.com>
2026-05-31 21:31:28 -0400

    Gateway connection between SupaBase and terminal has been established it works

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit
+from database import supabase
 
 app = QApplication(sys.argv)
 window = QMainWindow()
 window.setWindowTitle('Prodmax')
-label = QLabel('Hello, World!', parent=window)
-window.show()
+
+response = supabase.table("messages").select("*").execute()
+messages = response.data
+
+text = messages[0]['sender'] + ": " + messages[0]['content']
+
+text_edit = QTextEdit(parent=window)
+text_edit.setText(text)
+text_edit.setReadOnly(True)
+text_edit.setGeometry(50, 50, 900, 700)
+
 window.setGeometry(100, 100, 1000, 800)
+window.show()
 sys.exit(app.exec_())
