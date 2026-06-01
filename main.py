a79c6b03c851fd220c2b298928fc97ce79ab5985
Ryan Tran <188256973+ryantrain@users.noreply.github.com>
2026-06-01 00:33:16 -0400

    Set up some test ui's and buttons

--- main.py
+++ main.py
@@ -1,23 +1,63 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTextEdit
-import requests
+from PyQt5.QtWidgets import (
+    QApplication,
+    QLabel,
+    QMainWindow,
+    QStackedWidget,
+    QVBoxLayout,
+    QWidget,
+)
+from PyQt5 import uic
 
-#############################################################################
-# Setting up PyQt5 Application
-#############################################################################
+class Homepage(QMainWindow):
+
+    login_page: QStackedWidget
+    chat_page: QStackedWidget
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Prodmax')
+        self.setGeometry(100, 100, 800, 600)
+
+        self.stack = QStackedWidget()
+        self.setCentralWidget(self.stack)
+
+        self.login_page = self.create_login_page()
+        self.chat_page = self.create_chat_page()
+
+        self.stack.addWidget(self.login_page)
+        self.stack.addWidget(self.chat_page)
+
+        self.stack.setCurrentWidget(self.login_page)
+
+    def switch_to_chat(self):
+        self.stack.setCurrentWidget(self.chat_page)
+
+    def switch_to_login(self):
+        self.stack.setCurrentWidget(self.login_page)
+
+    def create_login_page(self):
+        page = uic.loadUi("test.ui")
+        layout = page.layout()
+        label = QLabel("Login Page", self)
+        label.setAttribute(self.underMouse(), True)
+        layout.addWidget(label)
+        page.button_1.clicked.connect(self.switch_to_chat)
+        return page
+
+    def create_chat_page(self):
+        page = uic.loadUi("test.ui")
+        layout = page.layout()
+        label = QLabel("Chat Page", self)
+        label.setAttribute(self.underMouse(), True)
+        layout.addWidget(label)
+        page.button_1.clicked.connect(self.switch_to_login)
+        return page
 
 app = QApplication(sys.argv)
-window = QMainWindow()
-window.setWindowTitle('Prodmax')
-window.setGeometry(100, 100, 1000, 800)
-
-response = requests.get("http://localhost:8000/messages")
-messages = response.json()
-
-text_edit = QTextEdit(parent=window)
-text_edit.setReadOnly(True)
-text_edit.setGeometry(50, 50, 900, 700)
-text_edit.setText("\n".join(messages))
-
+window = Homepage()
 window.show()
+window.raise_()
+window.activateWindow()
+sys.argv=[]
 sys.exit(app.exec_())
